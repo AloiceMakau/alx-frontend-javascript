08e5afddfdb96246884ddb1cbf9ae7f671b0f62f
Aloice Makau <110279759+AloiceMakau@users.noreply.github.com>
2025-10-15 00:14:18 +0300

    Update main.ts
    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,32 @@
 
+interface Teacher {
+  readonly firstName: string; 
+  readonly lastName: string;  
+  fullTimeEmployee: boolean;  
+  yearsOfExperience?: number;
+  location: string;           
+  [key: string]: any;       
+}
+
+
+const teacher1: Teacher = {
+  firstName: "Grace",
+  lastName: "Mwangi",
+  fullTimeEmployee: true,
+  location: "Nairobi",
+  contract: false, 
+};
+
+const teacher2: Teacher = {
+  firstName: "James",
+  lastName: "Odhiambo",
+  fullTimeEmployee: false,
+  yearsOfExperience: 5,
+  location: "Mombasa",
+  department: "Sciences", 
+};
+
+console.log(teacher1);
+console.log(teacher2);
+
+
